112c321b4fd8bddb34f66746619f3f9082e1aaf6
rahulrameshwarsharma <rahul.rameshwar.sharma@gmail.com>
2024-08-19 18:06:19 +0530

    counter project with condition of counter between 0 - 20 only

--- viteProjectOne/src/App.jsx
+++ viteProjectOne/src/App.jsx
@@ -15,6 +15,20 @@ function App() {
           <a href="#" className="font-bold text-4xl">&#8801;</a>
         </div>
       </nav>
+
+      <header className="w-full h-auto">
+        <img className="w-full hidden md:block" src="https://images.pexels.com/photos/27582996/pexels-photo-27582996/free-photo-of-a-statue-of-arco-da-rua-augusto-in-lisbon.jpeg" alt="" srcSet="" />
+        <img className="w-full md:hidden" src="https://images.pexels.com/photos/5031641/pexels-photo-5031641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcSet="" />
+      </header>
+
+      {/* Our student section */}
+
+      <div className="h-auto w-100 flex flex-wrap flex-col items-center text-center p-10">
+      <div className="w-full h-auto flex flex-wrap flex-col items-center">
+        <p className="text-indigo-900 font-bold text-3xl md:text-4xl text-center">{`"Pure Hardwork, No Shortcuts!"`}</p>
+        <div className="w-36 h-1 border-b-4 border-yellow-400 mt-2 rounded-2xl md:mt-4 mb-12"></div>
+      </div>
+      </div>
     </>
   )
 }
