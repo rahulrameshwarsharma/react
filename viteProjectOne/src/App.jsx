f31463a1d710a303fb37a6448f55092bede68c8c
rahulrameshwarsharma <rahul.rameshwar.sharma@gmail.com>
2024-08-23 01:31:34 +0530

    Basic Landing page using Tailwind Css

--- viteProjectOne/src/App.jsx
+++ viteProjectOne/src/App.jsx
@@ -1,4 +1,3 @@
-
 function App() {
 
   return (
@@ -17,8 +16,8 @@ function App() {
       </nav>
 
       <header className="w-full h-auto">
-        <img className="w-full hidden md:block" src="https://images.pexels.com/photos/27582996/pexels-photo-27582996/free-photo-of-a-statue-of-arco-da-rua-augusto-in-lisbon.jpeg" alt="" srcSet="" />
-        <img className="w-full md:hidden" src="https://images.pexels.com/photos/5031641/pexels-photo-5031641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" srcSet="" />
+        <img className="w-full hidden md:block" src="https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg" alt="" />
+        <img className="w-full md:hidden" src="https://buffer.com/library/content/images/size/w1200/2023/10/free-images.jpg" alt="" srcSet="" />
       </header>
 
       {/* Our student section */}
@@ -28,7 +27,91 @@ function App() {
         <p className="text-indigo-900 font-bold text-3xl md:text-4xl text-center">{`"Pure Hardwork, No Shortcuts!"`}</p>
         <div className="w-36 h-1 border-b-4 border-yellow-400 mt-2 rounded-2xl md:mt-4 mb-12"></div>
       </div>
+      <div className="w-full flex flex-wrap justify-evenly">
+        <div className="w-46 flex flex-col items-center mb-12">
+          <img className="w-44 h-44" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="text-3xl font-bold text-white">600+</p>
+          <p className="text-3xl font-bold text-gray-500">Different Courses</p>
+        </div>
+        <div className="w-46 flex flex-col items-center mb-12">
+        <img className="w-44 h-44" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="text-3xl font-bold text-white">700000+</p>
+          <p className="text-3xl font-bold text-gray-500">Students Enrolled</p>
+        </div>
+        <div className="w-46 flex flex-col items-center mb-12">
+        <img className="w-44 h-44" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="text-3xl font-bold text-white">10000+</p>
+          <p className="text-3xl font-bold text-gray-500">Successfull Transition</p>
+        </div>
       </div>
+      </div>
+
+      <div className="h-auto w-100 flex flex-wrap flex-col items-center text-center p-10">
+      <div className="w-full h-auto flex flex-wrap flex-col items-center">
+        <p className="text-indigo-900 font-bold text-3xl md:text-4xl text-center">{`"Our Products"`}</p>
+        <div className="w-36 h-1 border-b-4 border-yellow-400 mt-2 rounded-2xl md:mt-4 mb-12"></div>
+      </div>
+      <div className="w-full flex flex-wrap justify-evenly">
+        <div className="w-46 flex flex-col items-center mb-12">
+          <img className="w-44 h-44" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="text-3xl font-bold text-white">600+</p>
+          <p className="text-3xl font-bold text-gray-500">Different Courses</p>
+        </div>
+        <div className="w-46 flex flex-col items-center mb-12">
+        <img className="w-44 h-44" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="text-3xl font-bold text-white">700000+</p>
+          <p className="text-3xl font-bold text-gray-500">Students Enrolled</p>
+        </div>
+        <div className="w-46 flex flex-col items-center mb-12">
+        <img className="w-44 h-44" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="text-3xl font-bold text-white">700000+</p>
+          <p className="text-3xl font-bold text-gray-500">Students Enrolled</p>
+        </div>
+        <div className="w-46 flex flex-col items-center mb-12">
+        <img className="w-44 h-44" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="text-3xl font-bold text-white">10000+</p>
+          <p className="text-3xl font-bold text-gray-500">Successfull Transition</p>
+        </div>
+      </div>
+      </div>
+
+      <footer className="w-full bg-gray-900 px-4 text-white pt-8 flex flex-col md:flex-row flex-wrap justify-between md:px-12">
+        <div>
+        <img className="w-32 h-32" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+          <p className="my-4">Email Us: support@domain.commm</p>
+          <img className="w-32 h-32" src="https://pwskills.com/images/landing-page/statistics/books-icon.svg" alt="" />
+        </div>
+        
+        <div>
+          <div>
+            <h2 className="font-bold text-xl mt-4">Name Skills</h2>
+            <div className="w-32 h-1 border-b-2 border-yellow-400 rounded-2xl my-2"></div>
+            <p>About Us</p>
+            <p>FAQs</p>
+            <p>Privacy Policy</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="font-bold text-xl mt-4">Products</h2>
+            <div className="w-32 h-1 border-b-2 border-yellow-400 rounded-2xl my-2"></div>
+            <p>Skills Labs</p>
+            <p>Job Portal </p>
+            <p>Experience Portal</p>
+            <p>Become an affiliate</p>
+            <p>Hall of fame</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h2 className="font-bold text-xl mt-4">Links</h2>
+            <div className="w-32 h-1 border-b-2 border-yellow-400 rounded-2xl my-2"></div>
+            <p>Discord Channel</p>
+            <p>YouTube </p>
+            <p>Careers</p>
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
