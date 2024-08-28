9fd5981867f41a1991213586d576392965338f5f
rahulrameshwarsharma <rahul.rameshwar.sharma@gmail.com>
2024-08-29 04:46:51 +0530

    complete currency converter project uploaded

--- convertCurrency/README.md
+++ convertCurrency/README.md
@@ -1,2 +1,6 @@
 #   API for the currency converter
 -   https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/usd.json
+
+
+#   Icons
+-   used icons from here [Url of icon site](https://emojidb.org/arrows-emojis)
--- convertCurrency/src/App.jsx
+++ convertCurrency/src/App.jsx
@@ -1,15 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { InputBox } from './components';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
+
+import './App.css';
+import './styles/style.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [amount, setAmount] = useState(0)
+  const [from, setFrom] = useState("usd")
+  const [to, setTo] = useState("inr")
+  const [convertedAmount, setConvertedAmount] = useState(0)
+
+  if(amount < 0){
+    alert("Amount cannot be negative");
+  }
+
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to)
+    setTo(from)
+    setConvertedAmount(amount)
+    setAmount(convertedAmount)
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to])
+  }
 
   return (
     <>
+      <h1>Currency Converter</h1>
+
       <div className="mainWrapper">
-        <h1>Currency Converter</h1>
+        <div className="formWrapper">
+              <form onSubmit={(e) => {e.preventDefault(); convert(); }}>
+                  <div>
+                    <InputBox label="From " amount={amount} currencyOptions={options} onCurrencyChange={(currency) => setAmount(amount)} selectCurrency={from} onAmountChange={(amount) => setAmount(amount)} />
+                  </div>
+
+                  <div>
+                    <button type="button" onClick={swap}>swap ↑↓</button>
+                  </div>
+                  
+                  <div>
+                    <InputBox label="To " amount={convertedAmount} currencyOptions={options} onCurrencyChange={(currency) => setTo(currency)} selectCurrency={to} amountDisable />
+                  </div>
+                  <div>
+                    <button type="submit">Convert {from.toUpperCase()} to {to.toUpperCase}</button>
+                  </div>
+              </form>
+          </div>
       </div>
     </>
   )
